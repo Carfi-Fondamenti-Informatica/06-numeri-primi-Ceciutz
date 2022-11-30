360ee41abb653e94b1fe2fdfd6f44d106a6ddada
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-11-30 13:11:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool numero_primo(int a, int b){
+    a--;
+    if(a > 1){
+        if(b%a == 0){
+            return false;
+        }else {
+            numero_primo(a, b);
+        }
+    }else{
+        return true;
+    }
+}
