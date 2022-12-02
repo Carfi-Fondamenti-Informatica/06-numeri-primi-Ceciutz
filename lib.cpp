355e6d63be82cb6c66bb4cd89d195a3f3fd9b16e
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-02 13:47:51 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by Cecilia Utzeri on 02/12/2022
+//
 
+
+bool numero_primo(int x,int y){
+  x--;
+  if(x>1){
+    if(y%x==0) {
+      return false;
+    }else{
+      numero_primo(x,y);
+    }
+  }else {
+    return true;
+  }
+}
